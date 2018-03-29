ed2d0036bdac38dae3332ad26249498e7d927813
Misun Son <misun.son1022@gmail.com>
2018-03-29 17:02:01 -0400

    remove logger from production

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,24 @@
 var path = require("path");
 var webpack = require("webpack");
+var plugins = [];
+var devPlugins = [];
+
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
@@ -7,6 +26,10 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
   module: {
     loaders: [
