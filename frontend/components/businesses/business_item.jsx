b14a3438fb0ad665d70e4a4d2b25e363b732dcdd
Misun Son <misun.son1022@gmail.com>
2018-05-25 11:52:13 -0400

    styling biz items & show page

--- frontend/components/businesses/business_item.jsx
+++ frontend/components/businesses/business_item.jsx
@@ -22,47 +22,38 @@ class BusinessItem extends React.Component{
     } else{
       return (
         <div className="biz-item-wrapper">
-          <div className="biz-item-top">
-            <div className="item-left">
-              <Link to={`/business/${business.id}`}>
-                <img className="biz-idx-img" src={ business.photo } />
-              </Link>
-            </div>
-            <div className="item-middle">
-                <h1>{ idx }. { business.name }</h1>
-                <div className="review-rate-star">
-                  <Rating avgRating={ business.avg_rating } />
-                  &nbsp;&nbsp;&nbsp;
-                  <h4>{ business.review_cnt} Reviews</h4>
-                </div>
-
-                <div className="biz-header-info">
-                  <span className="price-range">$$ {business.price_range}</span>
-                    &nbsp;&nbsp;&nbsp;
-                  <span className="category">
-                    { business.category }</span>
-                </div>
-            </div>
-            <div className="item-right">
-              <div>{ business.address }</div>
-              <div>{ business.phone }</div>
-            </div>
+          <div className="item-grid-1">
+            <Link to={`/business/${business.id}`}>
+              <img className="biz-idx-img" src={ business.photo } />
+            </Link>
           </div>
-          <div className="biz-item-bottom">
-            <div className="item-left"></div>
-            <div className="item-wide-right">
-              <div className="review-idx-thumb">
-                <img src={ business.user_img } />
-              </div>
-              <div className="review-idx-content">
-                <p>
-                  { business.review.body }
-                </p>
-                <Link to={`/business/${business.id}`}>
-                  read more
-                </Link>
-              </div>
+          <div className="item-grid-2">
+            <h1>{ idx }. { business.name }</h1>
+            <div className="review-rate-index">
+              <Rating avgRating={ business.avg_rating } />
+              &nbsp;&nbsp;&nbsp;
+              <h4>{ business.review_cnt} Reviews</h4>
             </div>
+
+            <div className="biz-header-info">
+              <span className="price-range">$$ {business.price_range}</span>
+                &nbsp;&nbsp;&nbsp;
+              <span className="category">
+                { business.category }</span>
+            </div>
+
+
+          </div>
+          <div className="item-grid-3">
+            { business.address }
+          </div>
+          <div className="item-grid-4 review-idx-content">
+            <p>
+              { business.review.body }
+            </p>
+            <Link to={`/business/${business.id}`}>
+              read more
+            </Link>
           </div>
         </div>
       );
